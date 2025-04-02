f8b3e2b7fb0e328ae829eb6b451c5229634f1198
Esteban2511 <esteban.valenciab@uqvirtual.edu.co>
2025-04-01 22:24:10 -0500

    interfaces iniciales

--- co.edu.uniquindio.billetera_virtual_fx/src/main/java/co/edu/uniquindio/billetera_virtual/billetera_virtual_fx/viewcontroller/BilleteraAppController.java
+++ co.edu.uniquindio.billetera_virtual_fx/src/main/java/co/edu/uniquindio/billetera_virtual/billetera_virtual_fx/viewcontroller/BilleteraAppController.java
@@ -1,11 +1,15 @@
 package co.edu.uniquindio.billetera_virtual.billetera_virtual_fx.viewcontroller;
 
-import javafx.fxml.FXML;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class BilleteraAppController {
+
     @FXML
     private ResourceBundle resources;
 
@@ -13,7 +17,45 @@ public class BilleteraAppController {
     private URL location;
 
     @FXML
+    private Button btnIniciarSesion;
+
+    @FXML
+    private TextField txtContraseñaInicioSesion;
+
+    @FXML
+    private TextField txtCorreoElectronicoInicioSesion;
+
+    @FXML
+    private Button btnRecuperarDatos;
+
+    @FXML
+    private Button btnRegistrarse;
+
+    @FXML
+    void OnIniciarSesion(ActionEvent event) {
+
+    }
+
+
+    @FXML
+    void OnRecuperar(ActionEvent event) {
+
+
+    }
+
+    @FXML
+    void OnRegistrarse(ActionEvent event) {
+
+
+    }
+
+    @FXML
     void initialize() {
+        assert btnIniciarSesion != null : "fx:id=\"btnIniciarSesion\" was not injected: check your FXML file 'BilleteraApp.fxml'.";
+        assert txtContraseñaInicioSesion != null : "fx:id=\"txtContraseñaInicioSesion\" was not injected: check your FXML file 'BilleteraApp.fxml'.";
+        assert txtCorreoElectronicoInicioSesion != null : "fx:id=\"txtCorreoElectronicoInicioSesion\" was not injected: check your FXML file 'BilleteraApp.fxml'.";
+        assert btnRecuperarDatos != null : "fx:id=\"btnRecuperarDatos\" was not injected: check your FXML file 'BilleteraApp.fxml'.";
+        assert btnRegistrarse != null : "fx:id=\"btnRegistrarse\" was not injected: check your FXML file 'BilleteraApp.fxml'.";
 
     }
 }
