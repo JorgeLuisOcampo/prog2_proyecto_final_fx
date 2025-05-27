326b079347e10635ac417c9cc44cc6bbde026ee8
ZaeroS182 <117858979+JorgeLuisOcampo@users.noreply.github.com>
2025-05-27 13:50:31 -0500

    adiciones

--- co.edu.uniquindio.billetera_virtual_fx/src/main/java/co/edu/uniquindio/billetera_virtual/billetera_virtual_fx/utils/DataUtil.java
+++ co.edu.uniquindio.billetera_virtual_fx/src/main/java/co/edu/uniquindio/billetera_virtual/billetera_virtual_fx/utils/DataUtil.java
@@ -13,10 +13,10 @@ public class DataUtil {
         Categoria categoria1 = new Categoria(1, "Mercado", "Mercado", usuario1, null, billeteraVirtual);
         Categoria categoria2 = new Categoria(2, "Varios", "Gastos varios", usuario1, null, billeteraVirtual);
         Categoria categoria3 = new Categoria(3, "Gasolina", "Transporte", usuario1, null, billeteraVirtual);
-        Presupuesto presupuesto1 = new Presupuesto(1, "Alimento", 500000, usuario1, categoria1, billeteraVirtual);
-        Presupuesto presupuesto2 = new Presupuesto(2, "Varios", 300000, usuario1, categoria2, billeteraVirtual);
-        Presupuesto presupuesto3 = new Presupuesto(3, "Transporte", 150000, usuario1, categoria3, billeteraVirtual);
-        Cuenta cuenta = new Cuenta(1, "Banco Agrario", "54781542", usuario1, TipoCuenta.AHORRO, billeteraVirtual, presupuesto1);
+        Presupuesto presupuesto1 = new Presupuesto(1, "Alimento", 780000, usuario1, categoria1, billeteraVirtual);
+        Presupuesto presupuesto2 = new Presupuesto(2, "Varios", 520000, usuario1, categoria2, billeteraVirtual);
+        Presupuesto presupuesto3 = new Presupuesto(3, "Transporte", 190000, usuario1, categoria3, billeteraVirtual);
+        Cuenta cuenta1 = new Cuenta(1, "Banco Agrario", "54781542", usuario1, TipoCuenta.AHORRO, billeteraVirtual, presupuesto1);
         Cuenta cuenta2 = new Cuenta(2, "Banco Occidente", "26589454", usuario1, TipoCuenta.AHORRO, billeteraVirtual, presupuesto2);
         Cuenta cuenta3 = new Cuenta(3, "Banco Popular", "9874561", usuario1, TipoCuenta.CORRIENTE, billeteraVirtual, presupuesto3);
         billeteraVirtual.agregarUsuario(usuario1);
@@ -27,22 +27,25 @@ public class DataUtil {
         usuario1.agregarPresupuesto(presupuesto1);
         usuario1.agregarPresupuesto(presupuesto2);
         usuario1.agregarPresupuesto(presupuesto3);
-        usuario1.agregarCuenta(cuenta);
+        usuario1.agregarCuenta(cuenta1);
         usuario1.agregarCuenta(cuenta2);
         usuario1.agregarCuenta(cuenta3);
-
-        Transaccion transaccion1 = new Transaccion(1, LocalDate.of(2025,5,1), 700000, "Trabajo", usuario1, TipoTransaccion.DEPOSITO, cuenta, null, billeteraVirtual);
-        Transaccion transaccion2 = new Transaccion(2, LocalDate.of(2025,5,2), 25000, "Tarea", usuario1, TipoTransaccion.DEPOSITO, cuenta, null, billeteraVirtual);
-        Transaccion transaccion3 = new Transaccion(3, LocalDate.of(2025,5,3), 300000, "Consignación papás", usuario1, TipoTransaccion.DEPOSITO, cuenta, null, billeteraVirtual);
-        Transaccion transaccion4 = new Transaccion(4, LocalDate.of(2025,5,4), 150000, "Reembolso zapatos", usuario1, TipoTransaccion.DEPOSITO, cuenta2, null, billeteraVirtual);
-        Transaccion transaccion5 = new Transaccion(5, LocalDate.of(2025,5,5), 20000, "Apuesta ganada", usuario1, TipoTransaccion.DEPOSITO, cuenta2, null, billeteraVirtual);
-        Transaccion transaccion6 = new Transaccion(6, LocalDate.of(2025,5,6), 300000, "Consignación papás", usuario1, TipoTransaccion.DEPOSITO, cuenta2, null, billeteraVirtual);
-        Transaccion transaccion7 = new Transaccion(7, LocalDate.of(2025,5,7), 76000, "Pago deuda Camilo", usuario1, TipoTransaccion.DEPOSITO, cuenta2, null, billeteraVirtual);
-        Transaccion transaccion8 = new Transaccion(8, LocalDate.of(2025,5,8), 110000, "Pago deuda Sara", usuario1, TipoTransaccion.DEPOSITO, cuenta3, null, billeteraVirtual);
-        Transaccion transaccion9 = new Transaccion(9, LocalDate.of(2025,5,8), 15000, "Primer transferencia", usuario1, TipoTransaccion.TRANSFERENCIA, cuenta, cuenta2, billeteraVirtual);
-        Transaccion transaccion10 = new Transaccion(10, LocalDate.of(2025,5,9), 30000, "Primer retiro", usuario1, TipoTransaccion.RETIRO, cuenta2, null, billeteraVirtual);
-
-
+        Transaccion transaccion1 = new Transaccion(1, LocalDate.of(2025,5,3), 700000, "Salario compañia", usuario1, TipoTransaccion.DEPOSITO, cuenta1, null, billeteraVirtual);
+        Transaccion transaccion2 = new Transaccion(2, LocalDate.of(2025,5,5), 25000, "Inversiones", usuario1, TipoTransaccion.DEPOSITO, cuenta1, null, billeteraVirtual);
+        Transaccion transaccion3 = new Transaccion(3, LocalDate.of(2025,5,7), 300000, "Intereses CDT", usuario1, TipoTransaccion.DEPOSITO, cuenta1, null, billeteraVirtual);
+        Transaccion transaccion4 = new Transaccion(4, LocalDate.of(2025,5,11), 150000, "Devolución TEMU", usuario1, TipoTransaccion.DEPOSITO, cuenta2, null, billeteraVirtual);
+        Transaccion transaccion5 = new Transaccion(5, LocalDate.of(2025,5,12), 20000, "Venta de mercancía", usuario1, TipoTransaccion.DEPOSITO, cuenta2, null, billeteraVirtual);
+        Transaccion transaccion6 = new Transaccion(6, LocalDate.of(2025,5,13), 300000, "Consignación internacional", usuario1, TipoTransaccion.DEPOSITO, cuenta2, null, billeteraVirtual);
+        Transaccion transaccion7 = new Transaccion(7, LocalDate.of(2025,5,22), 76000, "Pago intereses Omer", usuario1, TipoTransaccion.DEPOSITO, cuenta2, null, billeteraVirtual);
+        Transaccion transaccion8 = new Transaccion(8, LocalDate.of(2025,5,22), 110000, "Pago intereses Consuelo", usuario1, TipoTransaccion.DEPOSITO, cuenta3, null, billeteraVirtual);
+        Transaccion transaccion9 = new Transaccion(9, LocalDate.of(2025,5,23), 15000, "Para cubrir gasolina", usuario1, TipoTransaccion.TRANSFERENCIA, cuenta1, cuenta2, billeteraVirtual);
+        Transaccion transaccion10 = new Transaccion(10, LocalDate.of(2025,5,23), 2000, "Calibrar llantas moto", usuario1, TipoTransaccion.TRANSFERENCIA, cuenta1, cuenta3, billeteraVirtual);
+        Transaccion transaccion11 = new Transaccion(11, LocalDate.of(2025,5,23), 30000, "Lavado y encerada moto", usuario1, TipoTransaccion.TRANSFERENCIA, cuenta2, cuenta3, billeteraVirtual);
+        Transaccion transaccion12 = new Transaccion(12, LocalDate.of(2025,5,23), 40000, "Comida para el fin de semana", usuario1, TipoTransaccion.TRANSFERENCIA, cuenta1, cuenta3, billeteraVirtual);
+        Transaccion transaccion13 = new Transaccion(13, LocalDate.of(2025,5,24), 15000, "Gasolina", usuario1, TipoTransaccion.RETIRO, cuenta2, null, billeteraVirtual);
+        Transaccion transaccion14 = new Transaccion(14, LocalDate.of(2025,5,24), 2000, "Calibrar Llantas", usuario1, TipoTransaccion.RETIRO, cuenta3, null, billeteraVirtual);
+        Transaccion transaccion15 = new Transaccion(15, LocalDate.of(2025,5,24), 30000, "Lavada en el Guadual", usuario1, TipoTransaccion.RETIRO, cuenta3, null, billeteraVirtual);
+        Transaccion transaccion16 = new Transaccion(16, LocalDate.of(2025,5,24), 40000, "Almuerzo en el Roble", usuario1, TipoTransaccion.RETIRO, cuenta1, null, billeteraVirtual);
         usuario1.agregarTransaccion(transaccion1);
         usuario1.agregarTransaccion(transaccion2);
         usuario1.agregarTransaccion(transaccion3);
@@ -53,6 +56,12 @@ public class DataUtil {
         usuario1.agregarTransaccion(transaccion8);
         usuario1.agregarTransaccion(transaccion9);
         usuario1.agregarTransaccion(transaccion10);
+        usuario1.agregarTransaccion(transaccion11);
+        usuario1.agregarTransaccion(transaccion12);
+        usuario1.agregarTransaccion(transaccion13);
+        usuario1.agregarTransaccion(transaccion14);
+        usuario1.agregarTransaccion(transaccion15);
+        usuario1.agregarTransaccion(transaccion16);
         return billeteraVirtual;
     }
 }
